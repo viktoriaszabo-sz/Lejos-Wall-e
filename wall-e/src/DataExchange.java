65c77c5d44cb89e01a6b3291a00b86e800906373
viktoriaszabo-sz <szaboviktoria.sz03@gmail.com>
2023-04-14 19:23:16 +0300

    dataexchange in progress
    dataexchange works, but for some reason starts doing the obstacle avoidance first, then linefollower. when put back to track, it works just fine (so like idk why it starts off so weirdly)

--- wall-e/src/DataExchange.java
+++ wall-e/src/DataExchange.java
@@ -1,33 +1,55 @@
 import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
 
-public class DataExchange extends Thread {
+public class DataExchange {
 
-    private boolean obstacleDetected = false; 
-    
+    //private boolean obstacleDetected = false; 
     //writing out the variables from each class
-    //color:
+    //linefollower
+    public static final int SPEED = 300;
+    public static final float BLACK_THRESHOLD = 0.1f;
     
+    //colorSensor
+    public static EV3ColorSensor colorSensor;
+    //public static final int[] colorSample = colorSensor.colorSample;
+    //public static final float[] colorSample = new float[colorSensor.getRedMode().sampleSize()];
+    public static final float[] colorSample = new float[1];
     
+    //obstacledetector
+    public static int TURN_ANGLE = 180; // angle to turn when avoiding obstacle
+	
+    //ultrasonic sensor 
+    public EV3UltrasonicSensor sonicSensor;
     
-    
-    public float[] ultraSample; //ultrasonic sensor sample
-    
+  
+    //private int CMD = 1; 
 
+    public DataExchange() {} //constructor
+    
+    public static void setColorSample(float[] sample)
+    {
+    	colorSample[0] = sample[0];
+    }
     
     
-    
-    
-    
-    
-    private int CMD = 1; 
-
-    public DataExchange() {
-
+  //public static float distance = UltrasonicSensor.distance;
+    public static float distance = 0;
+    public static void setDistance(float d)
+    {
+    	distance = d; //we put the distance into this DE method
+    }
+    public static float getDistance()
+    {
+    	return distance; //We will use this DE method for distance in OB
     }
 
+    
+    
+    
+    
     /* getters and setter */ 
 
-     public void setObstacleDetected (boolean status) {
+    /* public void setObstacleDetected (boolean status) {
         obstacleDetected = status; 
 
     }
@@ -39,6 +61,6 @@ public class DataExchange extends Thread {
     }
     public int getCMD() {
         return CMD; 
-    }
+    }*/
 
 }
--- wall-e/src/Main.java
+++ wall-e/src/Main.java
@@ -1,48 +1,34 @@
-/*import lejos.hardware.motor.EV3LargeRegulatedMotor; 
-import lejos.hardware.Button;
-import lejos.hardware.ev3.LocalEV3;
-import lejos.hardware.lcd.LCD;
-import lejos.hardware.Brick; 
-import lejos.hardware.BrickFinder; //connects to the body/brick 
-import lejos.hardware.motor.Motor;
-import lejos.hardware.port.MotorPort;
-import lejos.hardware.port.Port;
-import lejos.hardware.port.SensorPort;
-import lejos.hardware.port.TachoMotorPort;
-import lejos.hardware.sensor.EV3UltrasonicSensor;
-import lejos.robotics.SampleProvider;*/
-
-
 public class Main{
 
-    public static void main(String[] args) {
+	//private static final DataExchange DE = null;
+
+	//private static DataExchange DE;
+
+	public static void main(String[] args) {
       // Port colorPort = LocalEV3.get().getPort("S3");
-
       //lineFollower.followLine();
-
       //public static DataExchange DE; 
 
-      DataExchange DE = new DataExchange(); 
-
-      LineFollower lineFollower = new LineFollower(DE);
+	  DataExchange DE = new DataExchange();
+	
+      LineFollower lineFollower = new LineFollower();
+      ObstacleDetector detector1 = new ObstacleDetector();
+      ColorSensor colorSensor = new ColorSensor(); 
+      UltrasonicSensor ultraSonic = new UltrasonicSensor(); 
+      
+      colorSensor.start(); 
+      ultraSonic.start();
+      lineFollower.start();
+      detector1.start();
 
       //Port sensorPort = LocalEV3.get().getPort("S4");
       //EV3UltrasonicSensor usSensor = new EV3UltrasonicSensor(SensorPort.S4);
-        ObstacleDetector detector1 = new ObstacleDetector(DE);
       //ObstacleDetector detector1 = new ObstacleDetector(SensorPort.S4);
       //Thread detector2 = new Thread(detector1);
-
-
-
-        lineFollower.start();
-        detector1.start();
     }
-
-
     //private static DataExchange DE; 
      //private static LineFollower LFObjt; 
     //private static ObstacleDetector ODObj; 
-
 /*
     private static boolean waitForAnyPress() {
         // TODO Auto-generated method stub
