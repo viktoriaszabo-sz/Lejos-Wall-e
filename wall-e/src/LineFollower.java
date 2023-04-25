6e6fe9e45e51c7f4482f83c59226c0f5cc0c7b11
Viktória Szabina Szabó <szaboviktoria.sz03@gmail.com>
2023-04-25 11:05:45 +0300

    Update LineFollower.java

--- wall-e/src/LineFollower.java
+++ wall-e/src/LineFollower.java
@@ -6,7 +6,7 @@ import lejos.hardware.motor.Motor;
 import lejos.robotics.Color;
 import lejos.utility.Delay;
 
-//testing codeowner file permission
+//testing codeowner file permissions
 
 public class LineFollower extends Thread {
     public static final int SPEED = 300;
