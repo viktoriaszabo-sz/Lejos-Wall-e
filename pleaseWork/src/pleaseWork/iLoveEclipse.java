ec7b3475f14a3b67f8546309212159f1ff391322
Viktória Szabina Szabó <szaboviktoria.sz03@gmail.com>
2023-05-15 11:29:07 +0200

    Delete pleaseWork directory

--- pleaseWork/src/pleaseWork/iLoveEclipse.java
+++
@@ -1,7 +0,0 @@
-package pleaseWork;
-
-public class iLoveEclipse {
-
-	//I love Eclipse so much UwU 
-	
-}
--- pleaseWork/src/pleaseWork/iLoveMyLife.java
+++
@@ -1,6 +0,0 @@
-package pleaseWork;
-
-public class iLoveMyLife {
-
-	//My life is shining as bright as the Sun ^_^
-}
